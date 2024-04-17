9f001af348e3405c06d99a9e99b4ea3e37741d25
Jordi <im.jordiparadelo@gmail.com>
2024-04-17 12:05:53 +0200

    New build

--- src/libs/components/stackedSectionsAnimation.js
+++ src/libs/components/stackedSectionsAnimation.js
@@ -1,55 +1,115 @@
+import { parallaxAnimation } from "./index.js";
+
 export default function stackedSectionsAnimation(component) {
-	const sectionsArray = [... component.querySelectorAll('[data-component="parallax"]')];
+	const sectionsArray = [
+		...component.querySelectorAll('[data-component="parallax"]'),
+	];
 
-	if (!component || sectionsArray.length === 0 ) return;
+	if (!component || sectionsArray.length === 0) return;
 
-	const tl = gsap.timeline()
+	const tl = gsap.timeline();
 
 	function stackSections() {
-		gsap.set(sectionsArray, {
-			yPercent: (i) => i * -100,
-			opacity: (i) => i === 0 ? 1:  0,
-			position: 'sticky',
-			top: 0,
-		})
+		component.style.setProperty("--num-sections", sectionsArray.length);
+		gsap.set(sectionsArray, { opacity: 0 });
 	}
 
-	function animateSections() {
-		sectionsArray.forEach((section, index) => {
-			tl.to(section, {
-				opacity: index == 0 ? 0 : 1,
-				duration: 1,
-				scrollTrigger: {
-					trigger: section,
-					start: "top center",
-					end: "bottom center",
-					markers: true,
-					// toggleActions
-					// onEnter: () => tl.play(),
-					// onLeave: () => tl.reverse(),
-					// onEnterBack: () => tl.play(),
-					// onLeaveBack: () => tl.reverse(),
-				}
-			})
-		})
-		
-	}
+	async function animateSections() {
+		sectionsArray.forEach((section, index, array) => {
+			const startOffset = index * section.clientHeight;
+			const endOffset = startOffset + section.clientHeight;
 
-	function handleScroll() {
+			// Elements
+			const background = section.querySelector(".background_container");
+			const device = section.querySelector(".experiences_image-container");
+			const content = [
+				...section.querySelectorAll(".experiences_content-wrapper *"),
+			];
 
-		ScrollTrigger.create({
-			trigger: component,
-			start: "top center",
-			end: "bottom center",
-			markers: true,
-			animation: animateSections()
-		})
+			// Timeline
+			const animation = gsap.timeline({
+				defaults: {
+					// duration: 0.5,
+					// ease: "power3.out",
+				},
+			});
+
+			// animation
+			// 	// .from(background, {
+			// 	// 	opacity: 0,
+			// 	// 	duration: 0
+			// 	// })
+			// 	.to(section, {
+			// 		opacity: 1,
+			// 	},'start')
+			// 	.from(device, {
+			// 		opacity: 0,
+			// 	},'start')
+			// 	.from(content, {
+			// 		opacity: 0,
+			// 		yPercent: 20,
+			// 		stagger: 0.1,
+			// 	},'start')
+			// 	.to(content, {
+			// 		opacity: 0,
+			// 		yPercent: 20,
+			// 		duration: 0.2
+			// 	},'end')
+			// 	.to(device, {
+			// 		opacity: 0,
+			// 		duration: 0
+			// 	},'end')
+			// 	.to(background, {
+			// 		opacity: 0,
+			// 		duration: 0
+			// 	},'end')
+			// 	// .to(section, {
+			// 	// 	opacity: 0,
+			// 	// })
+
+			animation
+				.to(section, {
+					opacity: 1,
+				})
+				.to(section, {
+					opacity: (i) => {
+						const isLastSection = index === array.length - 1;
+
+						console.log({isLastSection, i})
+						return isLastSection ? 1 : 0;
+					},
+				});
+
+			// animation.fromTo(background, {
+			// 	opacity: 1
+			// }, {
+			// 	opacity: 0
+			// })
+
+			ScrollTrigger.create({
+				trigger: component,
+				start: `top+=${startOffset} bottom`,
+				end: `top+=${endOffset} bottom`,
+				// markers: true,
+				scrub: 1,
+				animation: animation,
+
+				// onEnter: () => gsap.set(section, { opacity: 1 }),
+				// onLeave: () => gsap.set(section, { opacity: 0 }),
+				// onEnterBack: () => gsap.set(section, { opacity: 1 }),
+				// onLeaveBack: () => gsap.set(section, { opacity: 0 }),
+
+				toggleActions: "play reverse play reverse",
+				// scrub: true,
+			});
+
+			tl.add(animation, index);
+		});
 	}
-	
 
 	function setUp() {
-		// handleScroll()
-		// stackSections()
+		stackSections();
+		animateSections();
 	}
 
 	function init() {
